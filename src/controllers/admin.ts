ed0c332fdf105df71c441f42ce032ad88440e0c6
niazy <niazy@alternative.agency>
2025-07-07 16:33:32 +0400

    integrate sql

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import Product from '../models/product';
 import { ProductType } from '../interfaces/Product';
+import { renderProductsPage } from '../helpers/render';
 
 const getAddProduct = (req: Request, res: Response, next: NextFunction) => {
   res.render('admin/edit-product', {
@@ -10,7 +11,7 @@ const getAddProduct = (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
-const getEditProduct = (req: Request, res: Response, next: NextFunction) => {
+const getEditProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
   // Product.fetchSingle(id, (product: ProductType | undefined) => {
   //   if (!product) {
@@ -24,38 +25,59 @@ const getEditProduct = (req: Request, res: Response, next: NextFunction) => {
   //     editing: true,
   //   });
   // });
-};
 
-const getAdminProduct = async (req: Request, res: Response, next: NextFunction) => {
-  const products = await Product.fetchAll();
-  res.render('shop/product-list', {
-    prods: products[0],
-    pageTitle: 'Admin Product',
-    path: '/admin/products',
+  const [[product]] = await Product.fetchSingle(id);
+  res.render('admin/edit-product', {
+    pageTitle: 'edit Product',
+    path: '/admin/edit-product',
+    product: product,
+    editing: true,
   });
 };
 
-const postAddProduct = (req: Request, res: Response, next: NextFunction) => {
+const getAdminProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await renderProductsPage(res, 'admin/product-list', 'Admin Product', '/admin/products');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postAddProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { title, price, description, imageUrl } = req.body;
   const product = new Product(null, title, price, description, imageUrl);
-  product.addProduct();
-  res.redirect('/');
+  try {
+    await product.addProduct();
+
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 };
 
-const postEditProduct = (req: Request, res: Response, next: NextFunction) => {
+const postEditProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { id, title, price, description, imageUrl } = req.body;
   const product = new Product(id, title, price, description, imageUrl);
-  product.updateProduct();
-  res.redirect('/');
+  try {
+    await product.updateProduct();
+
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+  // product.updateProduct();
+  // res.redirect('/');
 };
 
-const postDeleteProduct = (req: Request, res: Response, next: NextFunction) => {
+const postDeleteProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.body;
-  // console.log('ASd', id);
-  Product.deleteProduct(id);
-  res.redirect('/');
-  // const product = new Product(id, title, price, description, imageUrl);
-  // product.updateProduct();
+  try {
+    await Product.deleteProduct(id);
+
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,20 +1,6 @@
-import path from 'path';
-import fs from 'fs';
-import { ProductType } from '../interfaces/Product';
 import { v4 as uuidv4 } from 'uuid'; // install with `npm install uuid`
 import db from './../util/database';
 import { RowDataPacket } from 'mysql2';
-
-const path2 = path.join(path.dirname(require.main?.filename ?? ''), 'data', 'products.json');
-
-const getProductsFromFile = (cb: (products: ProductType[]) => void) => {
-  fs.readFile(path2, (err, fileContent) => {
-    if (err) {
-      return cb([]);
-    }
-    return cb(JSON.parse(fileContent.toString()));
-  });
-};
 
 class Product {
   title: string;
@@ -37,45 +23,44 @@ class Product {
     this.imageUrl = imageUrl;
   }
 
-  addProduct() {
-    getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(path2, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
-    });
+  async addProduct() {
+    const sql = `
+      INSERT INTO products (title, price, description, imageUrl)
+      VALUES (?, ?, ?, ?)
+    `;
+    const [result] = await db.execute(sql, [
+      this.title,
+      this.price,
+      this.description,
+      this.imageUrl,
+    ]);
+    return result;
   }
-  updateProduct() {
-    getProductsFromFile((products) => {
-      const foundIndex = products.findIndex((prod) => prod.id === this.id);
-      const updatedProducts = [...products];
-      updatedProducts[foundIndex] = this;
-      //   products.push(this);
-      fs.writeFile(path2, JSON.stringify(updatedProducts), (err) => {
-        console.log(err);
-      });
-    });
+  async updateProduct() {
+    const sql = `
+                UPDATE products
+                SET title=?, price=?, description=?, imageUrl=?
+                WHERE id= ?
+                `;
+    const [result] = await db.execute(sql, [
+      this.title,
+      this.price,
+      this.description,
+      this.imageUrl,
+      this.id,
+    ]);
+
+    return result;
   }
 
-  static deleteProduct(id: string) {
-    getProductsFromFile((products) => {
-      const updatedProducts = products.filter((product) => product.id !== id);
-      fs.writeFile(path2, JSON.stringify(updatedProducts), (err) => {
-        console.log(err);
-      });
-    });
+  static async deleteProduct(id: string) {
+    return db.execute(`DELETE  FROM products WHERE id =${id}`);
   }
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
   static async fetchSingle(id: string): Promise<[RowDataPacket[], any]> {
-    // getProductsFromFile((products: ProductType[]) => {
-    //   const foundProduct = products.find((product: ProductType) => {
-    //     return product.id === id;
-    //   });
-    //   cb(foundProduct);
-    // });
     return db.execute(`SELECT * FROM products WHERE id =${id}`);
   }
 }
